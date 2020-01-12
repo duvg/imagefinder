828e15cfe90db0c14a83117c252662092ab1ebd0
Duviel Garcia <duviel7@gmail.com>
2020-01-12 12:23:45 -0500

    Componete Buscador para el criterio de busqueda, componente de Error, Validacion del criterio de busqueda

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import Buscador from './components/Buscador';
 
 function App() {
   return (
     <div className="app container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de imagenes</p>
-
+        <Buscador />
       </div>
 
-      <div className="row justify-content-center"></div>
+      <div className="row justify-content-center">
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Buscador.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import Error from './Error';
+
+function Buscador () {
+
+    const [ terminoBusqueda, setTerminoBusqueda ] = useState('');
+    const [ error, setError ] = useState(false);
+
+
+    const buscarImagen = e => {
+        e.preventDefault();
+
+        // Validar el formulario
+        if(terminoBusqueda === '') {
+            setError(true);
+            return;
+        }
+
+        // Enviar el criterio de busqueda al componenten principal
+        setError(false);
+
+    }
+
+    return(
+        <form
+            onSubmit={buscarImagen}
+        >
+            <div className="row">
+                <div className="form-group col-md-8">
+                    <input 
+                        type="text" 
+                        className="form-control form-control-lg"
+                        placeholder="Busca una imagen, ejemplo: Videojuegos o Trabajo"
+                        onChange={e => setTerminoBusqueda(e.target.value)}
+                    />
+                </div>
+                <div className="form-group col-md-4">
+                    <input 
+                        type="submit" 
+                        className="btn btn-lg btn-danger btn-block"
+                        placeholder="Busca una imagen, ejemplo: Videojuegos o Trabajo"
+                        value="Buscar"
+                    />
+                </div>
+            </div>
+            {(error) ? <Error mensaje="Agreaga un criterio de busqueda"  /> : null }
+        </form>
+    );
+}
+
+export default Buscador;
